258371dd22a7c9ae58da0c4e7091d1f586f8dd5c
cloudwhite49554 <wengchenxi507@gmail.com>
2026-07-03 20:04:12 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
 
     QApplication a(argc, argv);
     MainWindow w;
-
+    w.setWindowTitle("Letter World");
     w.show();
     return QCoreApplication::exec();
 }
